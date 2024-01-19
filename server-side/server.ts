00e9f66dff594e8d9c6a69680056e20eccca38b7
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2024-01-19 17:41:21 +0100

    added gitignore

--- server-side/server.ts
+++ server-side/server.ts
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 import { connectToDatabase } from "./configurations/connectToDatabase";
 
 const port = 5000;
