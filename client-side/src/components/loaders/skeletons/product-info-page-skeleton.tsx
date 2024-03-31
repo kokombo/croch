85a4146546c4ad2197422fb59f260d1eba6b63d7
bowadao <samuelibrahim3029@gmail.com>
2024-03-31 16:18:00 +0100

    product info page mobile skeleton

--- client-side/src/components/loaders/skeletons/product-info-page-skeleton.tsx
+++ client-side/src/components/loaders/skeletons/product-info-page-skeleton.tsx
@@ -1,16 +1,19 @@
 import Skeleton from "./skeleton";
 
 const ProductInfoPageSkeleton = () => {
+
+  
   return (
     <section className="flex flex-col gap-4 py-10 px-[4.6%] ">
       <span className="h-8 block">
         <Skeleton classes="full width-45" />
       </span>
 
-      <div className="grid grid-cols-2 h-[428px] gap-3">
-        <div className="relative w-full bg-grey rounded-l-[20px]">
+      <div className="grid grid-rows-2 lg:grid-rows-none grid-cols-none lg:grid-cols-2 gap-3">
+        <div className="relative w-full bg-grey h-[356px] lg:h-[428px] rounded-t-xl lg:rounded-tr-none lg:rounded-l-[20px]">
           <Skeleton classes="width-100" />
         </div>
+
 
         <div className="grid grid-rows-2 gap-2">
           <div className="grid grid-cols-2 gap-2">
@@ -18,17 +21,17 @@ const ProductInfoPageSkeleton = () => {
               <Skeleton classes=" width-100" />
             </span>
 
-            <span className="relative w-full bg-grey rounded-tr-[20px]">
+            <span className="relative w-full bg-grey lg:rounded-tr-[20px]">
               <Skeleton classes=" width-100" />
             </span>
           </div>
 
           <div className="grid grid-cols-2 gap-2">
-            <span className="relative w-full bg-grey">
+            <span className="relative w-full bg-grey rounded-bl-xl lg:rounded-bl-none ">
               <Skeleton classes="width-100" />
             </span>
 
-            <span className="relative w-full bg-grey rounded-br-[20px]">
+            <span className="relative w-full bg-grey rounded-br-xl lg:rounded-br-[20px]">
               <Skeleton classes=" width-100" />
             </span>
           </div>
