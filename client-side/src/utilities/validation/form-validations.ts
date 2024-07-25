1dab0e6c3b2c48115250040762e8c6511b6dc9c6
bowadao <samuelibrahim3029@gmail.com>
2024-07-25 12:39:36 +0100

    yup input validation

--- client-side/src/utilities/validation/form-validations.ts
+++ client-side/src/utilities/validation/form-validations.ts
@@ -12,11 +12,18 @@ export const signupFormValidationSchema = Yup.object({
   email: Yup.string()
     .required("Enter your email address")
     .email("Invalid email format"),
-  firstName: Yup.string().required("First name is required "),
-  lastName: Yup.string().required("Last name is required"),
+  firstName: Yup.string()
+    .required("First name is required ")
+    .matches(/^\S(.*\S)?$/, "First name is required"),
+  lastName: Yup.string()
+    .required("Last name is required")
+    .matches(/^\S(.*\S)?$/, "Last name is required"),
   password: Yup.string()
     .required("Enter your password")
-    .min(6, "Password must be at least 6 characters"),
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      "At least 8 characters, a letter, a number and a special character."
+    ),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref("password")], "Passwords do not match")
     .required("Confirm your password"),
@@ -34,14 +41,21 @@ export const validationSchemas = [
   }),
 
   Yup.object().shape({
-    firstName: Yup.string().required("First name is required "),
-    lastName: Yup.string().required("Last name is required"),
+    firstName: Yup.string()
+      .required("First name is required ")
+      .matches(/^\S(.*\S)?$/, "First name is required"),
+    lastName: Yup.string()
+      .required("Last name is required")
+      .matches(/^\S(.*\S)?$/, "Last name is required"),
   }),
 
   Yup.object().shape({
     password: Yup.string()
       .required("Enter your password")
-      .min(6, "Password must be at least 6 characters"),
+      .matches(
+        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+        "At least 8 characters, a letter, a number and a special character."
+      ),
     confirmPassword: Yup.string()
       .oneOf([Yup.ref("password")], "Passwords do not match")
       .required("Confirm your password"),
@@ -52,11 +66,13 @@ export const creativeAccountSetupValidationSchema = Yup.object({
   brandName: Yup.string()
     .required("Enter your brand name.")
     .min(10, "Enter at least 10 characters.")
-    .max(50, "Max 50 characters."),
+    .max(50, "Max 50 characters.")
+    .matches(/^\S(.*\S)?$/, "Enter your brand name"),
   personalDescription: Yup.string()
     .required("Tell us about you and your brand.")
     .min(200, "Enter at least 200 characters.")
-    .max(500, "Max 500 characters."),
+    .max(500, "Max 500 characters.")
+    .matches(/^\S(.*\S)?$/, "Tell us about you and your brand"),
   yearsOfExperience: Yup.number()
     .required("Help customers understand how long you've been doing this.")
     .test("len", "Invalid input", (val) => val.toString().length < 3),
@@ -65,6 +81,7 @@ export const creativeAccountSetupValidationSchema = Yup.object({
       .required("Fun fact is required")
       .min(50, "Enter at least 50 characters")
       .max(150, "Max 150 characters")
+      .matches(/^\S(.*\S)?$/, "Fun fact is required")
   ),
   logo: Yup.mixed().required("Brand logo is required."),
 });
--- server-side/validators/formValidators.ts
+++ server-side/validators/formValidators.ts
@@ -12,11 +12,18 @@ export const signupFormValidationSchema = Yup.object({
   email: Yup.string()
     .required("Enter your email address")
     .email("Invalid email format"),
-  firstName: Yup.string().required("First name is required "),
-  lastName: Yup.string().required("Last name is required"),
+  firstName: Yup.string()
+    .required("First name is required ")
+    .matches(/^\S(.*\S)?$/, "First name is required"),
+  lastName: Yup.string()
+    .required("Last name is required")
+    .matches(/^\S(.*\S)?$/, "Last name is required"),
   password: Yup.string()
     .required("Enter your password")
-    .min(6, "Password must be at least 6 characters"),
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      "At least 8 characters, a letter, a number and a special character."
+    ),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref("password")], "Passwords do not match")
     .required("Confirm your password"),
@@ -25,7 +32,10 @@ export const signupFormValidationSchema = Yup.object({
 export const updatePasswordValidationSchema = Yup.object({
   password: Yup.string()
     .required("Enter your password")
-    .min(6, "Password must be at least 6 characters"),
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      "At least 8 characters, a letter, a number and a special character."
+    ),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref("password")], "Passwords do not match")
     .required("Confirm your password"),
@@ -35,11 +45,13 @@ export const creativeAccountSetupValidationSchema = Yup.object({
   brandName: Yup.string()
     .required("Enter your brand name.")
     .min(10, "Enter at least 10 characters.")
-    .max(50, "Max 50 characters."),
+    .max(50, "Max 50 characters.")
+    .matches(/^\S(.*\S)?$/, "Enter your brand name"),
   personalDescription: Yup.string()
     .required("Tell us about you and your brand.")
     .min(200, "Enter at least 200 characters.")
-    .max(500, "Max 500 characters."),
+    .max(500, "Max 500 characters.")
+    .matches(/^\S(.*\S)?$/, "Tell us about you and your brand"),
   yearsOfExperience: Yup.number()
     .required("Help customers understand how long you've been doing this.")
     .test("len", "Invalid input", (val) => val.toString().length < 3),
@@ -48,20 +60,31 @@ export const creativeAccountSetupValidationSchema = Yup.object({
       .required("Fun fact is required")
       .min(50, "Enter at least 50 characters")
       .max(150, "Max 150 characters")
+      .matches(/^\S(.*\S)?$/, "Fun fact is required")
   ),
 });
 
 export const uploadProductValidationSchema = Yup.object({
-  title: Yup.string().required("Producy title is required"),
-  availability: Yup.string().required("Product availability type is required"),
-  price: Yup.string().required("Product price is required"),
-  description: Yup.string().required("Product description is required"),
+  title: Yup.string()
+    .required("Product title is required")
+    .matches(/^\S(.*\S)?$/, "Product title is required"),
+  availability: Yup.string()
+    .required("Product availability type is required")
+    .matches(/^\S(.*\S)?$/, "Product availability type is required"),
+  price: Yup.string()
+    .required("Product price is required")
+    .matches(/^\S(.*\S)?$/, "Product price is required"),
+  description: Yup.string()
+    .required("Product description is required")
+    .matches(/^\S(.*\S)?$/, "Product description is required"),
   gender: Yup.string().required("Select one of the above"),
   nationwideDelivery: Yup.boolean().required(
     "Specify available delivery option"
   ),
   primaryLocation: Yup.object({
-    name: Yup.string().required("Choose primary delivery location"),
+    name: Yup.string()
+      .required("Choose primary delivery location")
+      .matches(/^\S(.*\S)?$/, "Choose primary delivery location"),
     minDeliveryDays: Yup.number().required(
       "Choose minimum order fulfilment days for primary delivery location"
     ),
@@ -69,7 +92,7 @@ export const uploadProductValidationSchema = Yup.object({
       "Choose maximum order fulfilment days for primary delivery location"
     ),
   }),
-  tag: Yup.string().required("Choose a the best product tag"),
+  tag: Yup.string().required("Choose a product tag"),
   photos: Yup.mixed().required("Select product photos to showcase your work"),
 });
 
@@ -77,9 +100,16 @@ export const adminSignupValidationSchema = Yup.object({
   email: Yup.string()
     .required("Enter your email address")
     .email("Invalid email format"),
-  firstName: Yup.string().required("First name is required "),
-  lastName: Yup.string().required("Last name is required"),
+  firstName: Yup.string()
+    .required("First name is required ")
+    .matches(/^\S(.*\S)?$/, "First name is required"),
+  lastName: Yup.string()
+    .required("Last name is required")
+    .matches(/^\S(.*\S)?$/, "Last name is required"),
   password: Yup.string()
     .required("Enter your password")
-    .min(6, "Password must be at least 6 characters"),
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      "At least 8 characters, a letter, a number and a special character."
+    ),
 });
